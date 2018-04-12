88e93afcd0a35cd9f77d427953aa1828821e6dce
groksana <groksana@gmail.com>
2018-04-12 14:23:09 +0300

    Remove old functionality

--- src/main/java/com/gromoks/onlinemart/handler/ConnectionException.java
+++
@@ -1,7 +0,0 @@
-package com.gromoks.onlinemart.handler;
-
-public class ConnectionException extends RuntimeException{
-    public ConnectionException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/gromoks/onlinemart/handler/FileLoadException.java
+++
@@ -1,7 +0,0 @@
-package com.gromoks.onlinemart.handler;
-
-public class FileLoadException extends RuntimeException{
-    public FileLoadException(String message) {
-        super(message);
-    }
-}
