59c7c688b1c29f76a20b9f2d2596903fa3028381
groksana <groksana@gmail.com>
2018-04-27 18:13:11 +0300

    Add using NamedParameterJdbcTemplate

--- src/main/java/com/gromoks/onlinemart/dao/jdbc/JdbcProductDao.java
+++ src/main/java/com/gromoks/onlinemart/dao/jdbc/JdbcProductDao.java
@@ -22,9 +22,9 @@ public class JdbcProductDao implements ProductDao {
 
     private static final String GET_ALL_PRODUCT_SQL = "SELECT ID, NAME, PRICE, PICTUREPATH, DESCRIPTION FROM PRODUCT";
 
-    private static final String GET_PRODUCT_BY_ID_SQL = "SELECT ID, NAME, PRICE, PICTUREPATH, DESCRIPTION FROM PRODUCT WHERE ID = ?";
+    private static final String GET_PRODUCT_BY_ID_SQL = "SELECT ID, NAME, PRICE, PICTUREPATH, DESCRIPTION FROM PRODUCT WHERE ID = :id";
 
-    private static final String ADD_PRODUCT_SQL = "INSERT INTO PRODUCT(NAME, PRICE, PICTUREPATH, DESCRIPTION) VALUES(?, ?, ?, ?)";
+    private static final String ADD_PRODUCT_SQL = "INSERT INTO PRODUCT(NAME, PRICE, PICTUREPATH, DESCRIPTION) VALUES(:name, :price, :picturepath, :description)";
 
     private static final String SEARCH_PRODUCT_BY_KEY_WORD = "SELECT ID, NAME, PRICE, PICTUREPATH, DESCRIPTION FROM PRODUCT WHERE NAME LIKE :name";
 
@@ -67,49 +67,39 @@ public class JdbcProductDao implements ProductDao {
         log.info("Start query to get product from DB by id = {}", productId);
         long startTime = System.currentTimeMillis();
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(GET_PRODUCT_BY_ID_SQL)) {
-
-            preparedStatement.setInt(1, productId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            Product product = new Product();
-            if (resultSet.next()) {
-                product = productRowMapper.mapRow(resultSet);
-            } else if (resultSet.next()) {
-                log.error("Constraint violation. We can't have several products per one id");
-                throw new RuntimeException("Constraint violation. We can't have several products per one id");
-            }
-
-            log.info("Finish query to get product by id from DB. It took {} ms", System.currentTimeMillis() - startTime);
-            return product;
+        Map<String, Object> parameterMap = new HashMap<>();
+        parameterMap.put("id", productId);
+        Product product = null;
+        try {
+            product = namedParameterJdbcTemplate.queryForObject(GET_PRODUCT_BY_ID_SQL, parameterMap, productRowMapper);
         } catch (SQLException e) {
             log.error("Issue during extract product by id from db ", e);
             throw new RuntimeException("Issue during extract product by id from db ", e);
         }
+
+        log.info("Finish query to get product by id from DB. It took {} ms", System.currentTimeMillis() - startTime);
+        return product;
     }
 
     @Override
     public int add(Product product) {
         log.info("Start query to add product {}", product);
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(ADD_PRODUCT_SQL, PreparedStatement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setString(1, product.getName());
-            preparedStatement.setDouble(2, product.getPrice());
-            preparedStatement.setString(3, product.getPicturePath());
-            preparedStatement.setString(4, product.getDescription());
-            preparedStatement.executeUpdate();
-
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            int key = resultSet.next() ? resultSet.getInt(1) : 0;
-
-            log.info("Finish query to add product");
-            return key;
+        Map<String, Object> parameterMap = new HashMap<>();
+        parameterMap.put("name", product.getName());
+        parameterMap.put("price", product.getPrice());
+        parameterMap.put("picturepath", product.getPicturePath());
+        parameterMap.put("description", product.getDescription());
+        int key;
+        try {
+            key = namedParameterJdbcTemplate.update(ADD_PRODUCT_SQL, parameterMap);
         } catch (SQLException e) {
             log.error("Issue during product insert to db ", e);
             throw new RuntimeException("Issue during product insert to db ", e);
         }
+
+        log.info("Finish query to add product with id - {}", key);
+        return key;
     }
 
     @Override
@@ -119,7 +109,13 @@ public class JdbcProductDao implements ProductDao {
 
         Map<String, Object> parameterMap = new HashMap<>();
         parameterMap.put("name", "%" + keyWord + "%");
-        List<Product> products = namedParameterJdbcTemplate.query(SEARCH_PRODUCT_BY_KEY_WORD, parameterMap, productRowMapper);
+        List<Product> products;
+        try {
+            products = namedParameterJdbcTemplate.query(SEARCH_PRODUCT_BY_KEY_WORD, parameterMap, productRowMapper);
+        } catch (SQLException e) {
+            log.error("Issue during search products by key word in db ", e);
+            throw new RuntimeException("Issue during search products by key word in db ", e);
+        }
 
         log.info("Finish query to search product from DB by keyWord. It took {} ms", System.currentTimeMillis() - startTime);
         return products;
